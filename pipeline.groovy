bef4e6f8af44236c276a8083c013f757eb3a00c1
robertmurray-exavalu <131782937+robertmurray-exavalu@users.noreply.github.com>
2023-08-02 19:35:54 -0400

    Add files via upload

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,25 +1,34 @@
-pipeline {
-    agent any
-    stages {
-        stage("pull") {
+pipelineJob('pipeline-demo') {
+    definition {
+        cps {
+            script('''
+                pipeline {
+                    agent any
+                    stages {
+                        stage("pull") {
             
-            steps {
-            shell '''
-            cd aws_demo
-            git(${env.GITREPO}, ${env.GITBRANCH})
-            '''
-            }
-        }
-        stage("build") {
-            steps {
-            shell '''
-            cd aws_demo
-            cd hello_world
-            compress-archive -DestinationPath ${env.DESINATION_PATH}  -Path ${PATH}
-            aws lambda update-function-code --function-name ${env.FUNCTION_NAME} --zip-file ${env.ZIP_FILE} --force
-            '''
-            }
-        }
+                            steps {
+                                shell '''
+                                cd aws_demo
+                                git(${env.GITREPO}, ${env.GITBRANCH})
+                                '''
+                            }
+                        }
+                        stage("build") {
+                            steps {
+                                shell '''
+                                cd aws_demo
+                                cd hello_world
+                                compress-archive -DestinationPath ${env.DESINATION_PATH}  -Path ${PATH}
+                                aws lambda update-function-code --function-name ${env.FUNCTION_NAME} --zip-file ${env.ZIP_FILE} --force
+                                '''
+                            }
+                        }
         
+                    }
+                }
+            '''.stripIndent())
+            sandbox()
+        }
     }
 }
