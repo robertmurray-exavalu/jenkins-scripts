43b855ad8f526061f27974c9d034fdfca9d89dce
robertmurray-exavalu <131782937+robertmurray-exavalu@users.noreply.github.com>
2023-08-02 19:14:07 -0400

    Add files via upload

--- job.groovy
+++ job.groovy
@@ -2,13 +2,13 @@ job('demoLambda-Seed-Job') {
 	description("Seed job with parameters for demolambda")
   parameters{
     parameters{
-        stringParam(name: 'GITREPO', defaultValue: 'https://github.com/robertmurray-exavalu/jenkins-scripts.git')
-        string(name: 'GITBRANCH', defaultValue: 'main')
-        string(name: 'DESTINATION_PATH', defaultValue: 'vsCodeCSVLambda.zip')
-        string(name: 'PATH', defaultValue: 'lambda_function.py')
-        string(name: 'FUNCTION_NAME', defaultValue: 'vsCodeCSVLambda')
-        string(name: 'ZIP_FILE', defaultValue: 'fileb://vsCodeCSVLambda.zip')
-        credentials(name: 'Lambda Access', defaultValue: 'AKIASJUDC4AAITRUG4SE (Lambda Access)')
+        stringParam('GITREPO', 'https://github.com/robertmurray-exavalu/jenkins-scripts.git')
+        stringParam('GITBRANCH', 'main')
+        stringParam('DESTINATION_PATH', 'vsCodeCSVLambda.zip')
+        stringParam('PATH', 'lambda_function.py')
+        stringParam('FUNCTION_NAME', 'vsCodeCSVLambda')
+        stringParam('ZIP_FILE', 'fileb://vsCodeCSVLambda.zip')
+        credentialsParam('Lambda Access', 'AKIASJUDC4AAITRUG4SE (Lambda Access)')
     }
   }
   scm {
