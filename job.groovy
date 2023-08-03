2cb35e7e9a72ff82c7becc66862ddc45d1b36d1c
robertmurray-exavalu <131782937+robertmurray-exavalu@users.noreply.github.com>
2023-08-03 10:06:04 -0400

    Add files via upload

--- job.groovy
+++ job.groovy
@@ -18,8 +18,33 @@ job('demoLambda-Seed-Job') {
   scm {
     git(GIT_REPO,GIT_BRANCH)
   }
+  definition {
     cps {
-        script(readFileFromWorkspace('pipeline.groovy'))
-        sandbox()
+        pipeline {
+            agent any
+            stages {
+                stage("pull") {
+            
+                    steps {
+                    shell '''
+                    cd aws_demo
+                    git(${env.GITREPO}, ${env.GITBRANCH})
+                    '''
+                    }
+                 }
+        stage("build") {
+            steps {
+            shell '''
+            cd aws_demo
+            cd hello_world
+            compress-archive -DestinationPath ${env.DESINATION_PATH}  -Path ${PATH}
+            aws lambda update-function-code --function-name ${env.FUNCTION_NAME} --zip-file ${env.ZIP_FILE} --force
+            '''
+            }
+        }
+        
     }
+}
+    }
+  }
 }
