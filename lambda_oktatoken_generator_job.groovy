ffdcda14f587b511c1eae0d06f9f8c38bec4f2ec
robertmurray-exavalu <131782937+robertmurray-exavalu@users.noreply.github.com>
2023-11-02 12:05:23 -0400

    Update lambda_oktatoken_generator_job.groovy

--- lambda_oktatoken_generator_job.groovy
+++ lambda_oktatoken_generator_job.groovy
@@ -163,7 +163,7 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
             stringParam('lambda_layer', 'Sample Lambda Layer', '')
             definition {
                 cps{
-                    script(readFileFromWorkspace('./lambda_okta_generator_pipeline.groovy'))
+                    script(readFileFromWorkspace('lambda_okta_generator_pipeline.groovy'))
                     sandbox()
                 }
             }
