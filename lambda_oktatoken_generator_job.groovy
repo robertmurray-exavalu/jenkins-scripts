b17e3beb065f7aa62ffedf2acfa07a6bd80a1686
robertmurray-exavalu <131782937+robertmurray-exavalu@users.noreply.github.com>
2023-11-03 18:33:12 -0400

    Update lambda_oktatoken_generator_job.groovy

--- lambda_oktatoken_generator_job.groovy
+++ lambda_oktatoken_generator_job.groovy
@@ -123,7 +123,7 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                 description('URL for Token Retrieval')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script('if(environment.equals("dev")){return["https://dev-04923793.okta.com/oauth2/default/v1/token"]} else if (environment.equals("prod")){return["https://copperpoint.okta.com/oauth2/default/v1/token"]}')
+                    script("if (environment.equals('dev')){return['https://dev-04923793.okta.com/oauth2/default/v1/token']} else if (environment.equals('prod')){return['https://copperpoint.okta.com/oauth2/default/v1/token']}")
                     fallbackScript('return["error"]')
                 }
                 referencedParameter('environment')
