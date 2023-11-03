24d1644b1b50d4e235b7c79fc176c2536d8114a5
robertmurray-exavalu <131782937+robertmurray-exavalu@users.noreply.github.com>
2023-11-03 17:57:17 -0400

    Update lambda_oktatoken_generator_job.groovy

--- lambda_oktatoken_generator_job.groovy
+++ lambda_oktatoken_generator_job.groovy
@@ -147,11 +147,12 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                     script('return ["us-west-2"]')
                 }
             }
+            //Figured out if you use the Teams Copy link, it doesn't paste it as plain text.
             activeChoiceParam('token_url'){
                 description('URL for Token Retrieval')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script("return['https://dev-04923793.okta.com/oauth2/default/v1/token']")
+                    script("if (environment.equals('dev')){return['https://dev-04923793.okta.com/oauth2/default/v1/token']} else if (environment.equals('prod')){return['https://copperpoint.okta.com/oauth2/default/v1/token']}")
                 }
             }
             // wReadonlyStringParameterDefinition {
