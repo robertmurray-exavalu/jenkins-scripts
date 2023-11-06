f6d06c3c89097b4647ffa5af96823f2271cf77bf
robertmurray-exavalu <131782937+robertmurray-exavalu@users.noreply.github.com>
2023-11-06 16:59:39 -0500

    Update lambda_oktatoken_generator_pipeline.groovy

--- lambda_oktatoken_generator_pipeline.groovy
+++ lambda_oktatoken_generator_pipeline.groovy
@@ -48,59 +48,59 @@ pipeline {
         }
     }
 }
-// def approval(environment) {
-// script{
-// wrap([$class: 'BuildUser']) {
-//         user = env.BUILD_USER_ID
+def approval(environment) {
+script{
+wrap([$class: 'BuildUser']) {
+        user = env.BUILD_USER_ID
      
-//     def approvers
-//     def user_approver
-//     if (dev_env){
-//         approvers = approvers_dev_env
-//         println "Please approve the deployment of the lambda function to ${environment}"
-//     }else if(uat_env){
-//         approvers = approvers_uat_env
-//         println "Please approve the deployment of the lambda function to ${environment}"
-//     }else if (prod_env){
-//         approvers = approvers_prod_env
-//         println "Please approve the deployment of the lambda function to ${environment}"
-//     }
-//         echo "approval list: ${approvers}"
-//         for (item in approvers){
-//         emailext (
-//             subject: "Waiting for your approval to deploy to ${environment} environment! Job: '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-//             body: "Pipeline started by: ${user}<br><a href=${BUILD_URL}input>Click here  to approve/abort</a>",
-//             to: "${item}", mimeType: 'text/html'
-//         )
-//         }
-//         timeout(time: 60, unit: 'MINUTES'){
-//          userInput = input id: 'userInput',
-//                               message: 'Proceed for Deployment',
-//                               submitterParameter: 'submitter',
-//                               submitter: 'Approval',
-//                               parameters: [[$class: 'BooleanParameterDefinition', defaultValue: true,, description:'', name:'Approved']]
-//         }
-//         user_approver = userInput['submitter']
-//         if("${approvers}".contains("${user_approver}") && ("${user_approver}"!="${user}")){
-//              echo 'Approved'
-//         }else{
-//              //error_message('You cannot approve your own pipeline or you should be part of approval group')
-//         }
-// }
-// }
-// }
+    def approvers
+    def user_approver
+    if (dev_env){
+        approvers = approvers_dev_env
+        println "Please approve the deployment of the lambda function to ${environment}"
+    }else if(uat_env){
+        approvers = approvers_uat_env
+        println "Please approve the deployment of the lambda function to ${environment}"
+    }else if (prod_env){
+        approvers = approvers_prod_env
+        println "Please approve the deployment of the lambda function to ${environment}"
+    }
+        echo "approval list: ${approvers}"
+        for (item in approvers){
+        emailext (
+            subject: "Waiting for your approval to deploy to ${environment} environment! Job: '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+            body: "Pipeline started by: ${user}<br><a href=${BUILD_URL}input>Click here  to approve/abort</a>",
+            to: "${item}", mimeType: 'text/html'
+        )
+        }
+        timeout(time: 60, unit: 'MINUTES'){
+         userInput = input id: 'userInput',
+                              message: 'Proceed for Deployment',
+                              submitterParameter: 'submitter',
+                              submitter: 'Approval',
+                              parameters: [[$class: 'BooleanParameterDefinition', defaultValue: true,, description:'', name:'Approved']]
+        }
+        user_approver = userInput['submitter']
+        if("${approvers}".contains("${user_approver}") && ("${user_approver}"!="${user}")){
+             echo 'Approved'
+        }else{
+             //error_message('You cannot approve your own pipeline or you should be part of approval group')
+        }
+}
+}
+}
 
-// def failure(stage){
-// script{
-// wrap([$class: 'BuildUser']) {
-//         user = env.BUILD_USER_ID
-//      emailext attachLog: true, body: "Pipeline started by: ${user}<br> Failed in ${stage} Stage ${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} <br> More info at: ${env.BUILD_URL}", subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}:${env.BUILD_NUMBER}", to: "$notification_list", mimeType: 'text/html'
-// }
-// }
-// }
+def failure(stage){
+script{
+wrap([$class: 'BuildUser']) {
+        user = env.BUILD_USER_ID
+     emailext attachLog: true, body: "Pipeline started by: ${user}<br> Failed in ${stage} Stage ${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} <br> More info at: ${env.BUILD_URL}", subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}:${env.BUILD_NUMBER}", to: "$notification_list", mimeType: 'text/html'
+}
+}
+}
 
-// def error_message(message)
-// {
-//     currentBuild.result = 'ABORTED'
-//     error "${message}, Build is aborted"
-// }
+def error_message(message)
+{
+    currentBuild.result = 'ABORTED'
+    error "${message}, Build is aborted"
+}
