9314ed88cae52828731e0b27b75b894079450ac8
robertmurray-exavalu <131782937+robertmurray-exavalu@users.noreply.github.com>
2023-11-07 12:50:48 -0500

    Update lambda_oktatoken_generator_job.groovy

--- lambda_oktatoken_generator_job.groovy
+++ lambda_oktatoken_generator_job.groovy
@@ -17,14 +17,14 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                 description('Select the environment')
                 choiceType('RADIO')
                 groovyScript{
-                    script('return ["dev", "prod"]')
+                    script('return ["nonprd", "prod"]')
                 }
             }
             activeChoiceReactiveParam('SecurityGroup') {
                 description('')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script("if (environment.equals('dev')){return['sg-0e03e244b6239de0d:selected']} else if (environment.equals('prod')){return['sg-0368890663d3ef3a4:selected']}")
+                    script("if (environment.equals('nonprd')){return['sg-0e03e244b6239de0d:selected']} else if (environment.equals('prod')){return['sg-0368890663d3ef3a4:selected']}")
                     fallbackScript('return["error"]')
                 }
                 referencedParameter('environment')
@@ -33,7 +33,7 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                 description('')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script("if (environment.equals('dev')){return['subnet-08840e286a6ef0a44:selected']} else if (environment.equals('prod')){return['subnet-0d4d44c8f81a888d4:selected']}")
+                    script("if (environment.equals('nonprd')){return['subnet-08840e286a6ef0a44:selected']} else if (environment.equals('prod')){return['subnet-0d4d44c8f81a888d4:selected']}")
                     fallbackScript('return["error"]')
                 }
                 referencedParameter('environment')
@@ -42,7 +42,7 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                 description('')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script("if (environment.equals('dev')){return['subnet-0af7e31a81b8d8810:selected']} else if (environment.equals('prod')){return['subnet-0178d984fb3c6ea23:selected']}")
+                    script("if (environment.equals('nonprd')){return['subnet-0af7e31a81b8d8810:selected']} else if (environment.equals('prod')){return['subnet-0178d984fb3c6ea23:selected']}")
                     fallbackScript('return["error"]')
                 }
                 referencedParameter('environment')
@@ -51,7 +51,7 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                 description('')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script("if (environment.equals('dev')){return['subnet-0911ce3c16fa99d77:selected']} else if (environment.equals('prod')){return['subnet-009a020f928b1f8a6:selected']}")
+                    script("if (environment.equals('nonprd')){return['subnet-0911ce3c16fa99d77:selected']} else if (environment.equals('prod')){return['subnet-009a020f928b1f8a6:selected']}")
                     fallbackScript('return["error"]')
                 }
                 referencedParameter('environment')
@@ -60,7 +60,7 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                 description('')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script("if (environment.equals('dev')){return['developer:selected']} else if (environment.equals('prod')){return['main:selected']}")
+                    script("if (environment.equals('nonprd')){return['developer:selected']} else if (environment.equals('prod')){return['main:selected']}")
                     fallbackScript('return["error"]')
                 }
                 referencedParameter('environment')
@@ -69,7 +69,7 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                 description('Name of S3 Bucket for Artifact Packaging')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script("if (environment.equals('dev')){return['${job_name}-dev:selected']} else if (environment.equals('prod')){return['${job_name}-prod:selected']}")
+                    script("if (environment.equals('nonprd')){return['${job_name}-nonprd:selected']} else if (environment.equals('prod')){return['${job_name}-prod:selected']}")
                     fallbackScript('return["error"]')
                 }
                 referencedParameter('environment')
@@ -78,7 +78,7 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                 description('Name of Cloudformation Stack')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script("if (environment.equals('dev')){return['${job_name}-dev:selected']} else if (environment.equals('prod')){return['${job_name}-prod:selected']}")
+                    script("if (environment.equals('nonprd')){return['${job_name}-nonprd:selected']} else if (environment.equals('prod')){return['${job_name}-prod:selected']}")
                     fallbackScript('return["error"]')
                 }
                 referencedParameter('environment')
@@ -87,7 +87,7 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                 description('')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script("if (environment.equals('dev')){return['arn:aws:iam::314704840185:role/cpic-integ-nonprd:selected']} else if (environment.equals('prod')){return['arn:aws:iam::448503678883:role/cpic-integ-prod:selected']}")
+                    script("if (environment.equals('nonprd')){return['arn:aws:iam::314704840185:role/cpic-integ-nonprd:selected']} else if (environment.equals('prod')){return['arn:aws:iam::448503678883:role/cpic-integ-prod:selected']}")
                     fallbackScript('return["error"]')
                 }
                 referencedParameter('environment')
@@ -96,7 +96,7 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                 description('')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script("if (environment.equals('dev')){return['${job_name}-nonprd:selected']} else if (environment.equals('prod')){return['${job_name}-prod:selected']}")
+                    script("if (environment.equals('nonprd')){return['${job_name}-nonprd:selected']} else if (environment.equals('prod')){return['${job_name}-prod:selected']}")
                     fallbackScript('return["error"]')
                 }
                 referencedParameter('environment')
@@ -105,7 +105,7 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                 description('')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script("if (environment.equals('dev')){return['NonProd:selected']} else if (environment.equals('prod')){return['Prod:selected']}")
+                    script("if (environment.equals('nonprd')){return['NonProd:selected']} else if (environment.equals('prod')){return['Prod:selected']}")
                     fallbackScript('return["error"]')
                 }
                 referencedParameter('environment')
@@ -114,7 +114,7 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                 description('')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script("if (environment.equals('dev')){return['AWS service account:selected']} else if (environment.equals('prod')){return['aws-service-account-prod:selected']}")
+                    script("if (environment.equals('nonprd')){return['AWS service account:selected']} else if (environment.equals('prod')){return['aws-service-account-prod:selected']}")
                     fallbackScript('return["error"]')
                 }
                 referencedParameter('environment')
@@ -155,7 +155,7 @@ def lambdaDeploymentJobs(job_name, gitURL, FunctionName){
                 description('URL for Token Retrieval')
                 choiceType('SINGLE_SELECT')
                 groovyScript {
-                    script("if (environment.equals('dev')){return['https://dev-04923793.okta.com/oauth2/default/v1/token']} else if (environment.equals('prod')){return['https://copperpoint.okta.com/oauth2/default/v1/token']}")
+                    script("if (environment.equals('nonprd')){return['https://dev-04923793.okta.com/oauth2/default/v1/token']} else if (environment.equals('prod')){return['https://copperpoint.okta.com/oauth2/default/v1/token']}")
                     fallbackScript('return["error"]')
                 }
                 referencedParameter('environment')
