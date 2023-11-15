0f95140bd16d2fa380475bb0c39ff932e053fadb
robertmurray-exavalu <131782937+robertmurray-exavalu@users.noreply.github.com>
2023-11-15 10:14:35 -0500

    Update lambda_oktatoken_generator_pipeline.groovy

--- lambda_oktatoken_generator_pipeline.groovy
+++ lambda_oktatoken_generator_pipeline.groovy
@@ -14,33 +14,34 @@ pipeline {
             steps{
                 deleteDir()
                 approval(params.environment)
-                git branch: "main",
-                credentialsId: '8dfc0659-2d5a-411f-8eba-1c21f5a41feb',
-                url: "git@github.com:robertmurray-exavalu/testJenkinsDeploy.git"
+                git branch: "${gitBranch}",
+                credentialsId: 'c109308d-28d6-4bed-bd08-60f713447612',
+                url: "${gitURL}"
             }
         }
-        // stage('Python Test') {
-        //     steps{
-        //         sh '''
-        //         python3 -m pytest --cov --cov-report=html --cov-fail-under=80
-        //         '''
-        //     }
-        //     post {
-        //         always{
-        //             publishHTML (target: [
-        //             allowMissing: false,
-        //             alwaysLinkToLastBuild: false,
-        //             keepAll: true,
-        //             reportDir: 'htmlcov',
-        //             reportFiles: '*.html',
-        //             reportName: "Pytest-Report"
-        //             ])
-        //         }
-        //         failure {
-        //             failure('Python Test')
-        //         }
-        //     }
-        // }
+        stage('Python Test') {
+            steps{
+                sh '''
+                python3 -m pip install -r requirements.txt \
+                python3 -m pytest --cov --cov-report=html --cov-fail-under=80
+                '''
+            }
+            post {
+                always{
+                    publishHTML (target: [
+                    allowMissing: false,
+                    alwaysLinkToLastBuild: false,
+                    keepAll: true,
+                    reportDir: 'htmlcov',
+                    reportFiles: '*.html',
+                    reportName: "Pytest-Report"
+                    ])
+                }
+                failure {
+                    failure('Python Test')
+                }
+            }
+        }
         stage('Build & Deploy'){
             steps{
                 samDeploy([credentialsId: "$cred_id", kmsKeyId: '', outputTemplateFile: 'template-packaged.yaml', parameters: [[key: 'SecurityGroup', value: "$SecurityGroup"], [key: 'SubnetA', value: "$SubnetA"], [key: 'SubnetB', value: "$SubnetB"], [key: 'SubnetC', value: "$SubnetC"], [key: 'FunctionName', value: "$FunctionName"], [key: 'MemorySize', value: "$MemorySize"], [key: 'Timeout', value: "$Timeout"], [key: 'Role', value: "$role"], [key: 'Architecture', value: "$Architecture"], [key: 'Runtime', value: "$Lambda_Runtime"], [key: 'LambdaLayer', value: "$lambda_layer"], [key: 'LogCollection', value: "$Log_Collection"], [key: 'LambdaEnv', value: "$Lambda_Env"],[key: 'scope', value: "$token_scope"],[key: 'GrantType', value: "$token_grant_type"],[key: 'SecretName', value: "$token_secret_name"],[key: 'RegionName', value: "$token_region"], [key: 'url', value: "$token_url"]], region: 'us-west-2', roleArn: '', s3Bucket: "$s3_bucket", s3Prefix: '', stackName: "$stack_name", templateFile: 'template.yaml'])
